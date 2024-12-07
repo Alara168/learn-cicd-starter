a24b3a7eb4e0688fcc52a132ea9a4257ef9820ac
Alara168 <alejandrolara168@gmail.com>
2024-12-07 12:23:59 +0100

    fixes

--- handler_user.go
+++ handler_user.go
@@ -17,20 +17,31 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 	type parameters struct {
 		Name string `json:"name"`
 	}
+
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
+
+	// Decode request body
 	err := decoder.Decode(&params)
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		respondWithError(w, http.StatusBadRequest, "Couldn't decode parameters: "+err.Error())
 		return
 	}
 
+	// Validate input
+	if params.Name == "" {
+		respondWithError(w, http.StatusBadRequest, "Name cannot be empty")
+		return
+	}
+
+	// Generate API key
 	apiKey, err := generateRandomSHA256Hash()
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't gen apikey")
+		respondWithError(w, http.StatusInternalServerError, "Couldn't generate API key: "+err.Error())
 		return
 	}
 
+	// Create user in database
 	err = cfg.DB.CreateUser(r.Context(), database.CreateUserParams{
 		ID:        uuid.New().String(),
 		CreatedAt: time.Now().UTC().Format(time.RFC3339),
@@ -38,25 +49,30 @@ func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request)
 		Name:      params.Name,
 		ApiKey:    apiKey,
 	})
+
 	if err != nil {
-		log.Println(err)
+		log.Printf("Error creating user: %v", err)
 		respondWithError(w, http.StatusInternalServerError, "Couldn't create user")
 		return
 	}
 
+	// Retrieve created user
 	user, err := cfg.DB.GetUser(r.Context(), apiKey)
 	if err != nil {
-		log.Println(err)
+		log.Printf("Error retrieving user after creation: %v", err)
 		respondWithError(w, http.StatusInternalServerError, "Couldn't get user")
 		return
 	}
 
+	// Convert user to response format
 	userResp, err := databaseUserToUser(user)
 	if err != nil {
-		log.Println(err)
+		log.Printf("Error converting user to response format: %v", err)
 		respondWithError(w, http.StatusInternalServerError, "Couldn't convert user")
 		return
 	}
+
+	// Respond with created user information
 	respondWithJSON(w, http.StatusCreated, userResp)
 }
 
